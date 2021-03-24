821c01644ce6d1d91b66c64f2b9b557c0bee3322
Joshua May <notjosh@gmail.com>
2021-03-24 14:32:43 +0100

    theme viz

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import { Universe as UniverseType } from '../data/universe/types';
 import Location from './Location';
 
+import theme from '../data/assets/theme.json';
+
 type Props = UniverseType;
 
 const List: React.FC<Props> = ({ nodes }) => {
   return (
     <div>
-      {nodes.map((node, idx) => (
-        <Location key={idx} {...node} />
+      {Object.keys(theme).map((key) => (
+        <div>
+          <p>
+            {key} (<code>{(theme as any)[key].hex}</code>)
+          </p>
+          <div
+            style={{
+              backgroundColor: (theme as any)[key].hex,
+              width: 100,
+              height: 100,
+            }}
+          ></div>
+        </div>
       ))}
+      {/* {nodes.map((node, idx) => (
+        <Location key={idx} {...node} />
+      ))} */}
     </div>
   );
 };
